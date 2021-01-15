49bb74f02fc2aedbdd926fd26add684242ebc06c
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2021-01-15 16:23:31 -0300

    Cors configuration

--- src/main/java/com/solucitiva/solucitiva/auth/auth/AuthorizationServerConfig.java
+++ src/main/java/com/solucitiva/solucitiva/auth/auth/AuthorizationServerConfig.java
@@ -2,6 +2,7 @@ package com.solucitiva.solucitiva.auth.auth;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -15,11 +16,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     private PasswordEncoder passwordEncoder;
     private AuthenticationManager authenticationManager;
+    private UserDetailsService userDetailsService;
 
 
-    public AuthorizationServerConfig(PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager){
+    public AuthorizationServerConfig(PasswordEncoder passwordEncoder,
+                                     AuthenticationManager authenticationManager,
+                                     UserDetailsService userDetailsService){
         this.passwordEncoder= passwordEncoder;
         this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
     }
 
 
@@ -29,7 +34,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 inMemory()
                     .withClient("Contabil-web")
                         .secret(passwordEncoder.encode("web123"))
-                        .authorizedGrantTypes("password")
+                        .authorizedGrantTypes("password", "refresh_token")
                         .scopes("write","read")
                         .accessTokenValiditySeconds(60 * 60 * 6)
                 .and()
@@ -45,6 +50,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
     }
 }
