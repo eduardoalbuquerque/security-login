27b2450c899716818a0cae2c02674ae547cd4f19
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2021-01-22 19:53:21 -0300

    correcao oauth token

--- src/main/java/com/solucitiva/solucitiva/auth/auth/AuthorizationServerConfig.java
+++ src/main/java/com/solucitiva/solucitiva/auth/auth/AuthorizationServerConfig.java
@@ -1,11 +1,7 @@
 package com.solucitiva.solucitiva.auth.auth;
 
-import com.nimbusds.jose.shaded.json.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,14 +10,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-
-import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.text.ParseException;
 
 @Configuration
 @EnableAuthorizationServer
@@ -38,34 +26,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        if(!isValid("6Lf72TQaAAAAAJFe2JG9P0nhmmdsrQefiddKkUdt")){
-//            return;
-//        }
         clients
                 .inMemory()
-                .withClient("contabil-web")
+                .withClient("contabil")
                 .secret(passwordEncoder.encode("web123"))
                 .authorizedGrantTypes("password", "refresh_token")
                 .scopes("write", "read")
                 .accessTokenValiditySeconds(6 * 60 * 60)// 6 horas
-                .refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
-
-                .and()
-                .withClient("foodanalytics")
-                .secret(passwordEncoder.encode("food123"))
-                .authorizedGrantTypes("authorization_code")
-                .scopes("write", "read")
-                .redirectUris("http://www.foodanalytics.local:8082")
-
-                .and()
-                .withClient("faturamento")
-                .secret(passwordEncoder.encode("faturamento123"))
-                .authorizedGrantTypes("client_credentials")
-                .scopes("write", "read")
-
-                .and()
-                .withClient("checktoken")
-                .secret(passwordEncoder.encode("check123"));
+                .refreshTokenValiditySeconds(60 * 24 * 60 * 60);// 60 dias
     }
 
     @Override
@@ -80,65 +48,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService)
                 .reuseRefreshTokens(false);
-    }
-
-    public boolean isValid(String clientRecaptchaResponse) throws IOException, ParseException, JSONException {
-        final String RECAPTCHA_SERVICE_URL = "https://www.google.com/recaptcha/api/siteverify";
-        final String SECRET_KEY = "6Lf72TQaAAAAAI2lW3dkZXCOpzuDy5cYAWIBPxDo";
-
-        if(clientRecaptchaResponse == null || "".equals(clientRecaptchaResponse)){
-            return false;
-        }
-
-        //conecta a url do google
-        URL url = new URL(RECAPTCHA_SERVICE_URL);
-        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-
-        String postParams =
-                "secret=" + SECRET_KEY +
-                "&response=" + clientRecaptchaResponse;
-
-        //enviar POST para o google reCAPTCHA sever
-        connection.setDoOutput(true);
-        DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
-        dataOutputStream.writeBytes(postParams);
-        dataOutputStream.flush();
-        dataOutputStream.close();
-
-        int responseCode = connection.getResponseCode();
-
-        //apagar - apenas para visualizar no console
-        System.out.println("Post parameters: " + postParams);
-        System.out.println("Response Code: " + responseCode);
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                connection.getInputStream()));
-
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        //apagar - apenas para visualizar no console
-        System.out.println(response.toString());
-
-        System.out.println(response.toString());
-
-        //Parse JSON-response
-        JSONObject jsonObject = new JSONObject(response.toString());
-
-        Boolean success = (Boolean) jsonObject.get("success");
-        Double score = (Double) jsonObject.get("score");
-
-        System.out.println("success : " + success);
-        System.out.println("score : " + score);
-
-        //result should be sucessfull and spam score above 0.5
-        return (success && score >= 0.5);
-
     }
 
 }
--- src/main/java/com/solucitiva/solucitiva/auth/auth/CorsConfig.java
+++ src/main/java/com/solucitiva/solucitiva/auth/auth/CorsConfig.java
@@ -1,16 +1,16 @@
 package com.solucitiva.solucitiva.auth.auth;
-import java.util.Collections;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@Component
+import java.util.Collections;
+
+@Configuration
 public class CorsConfig {
 
     @Bean
