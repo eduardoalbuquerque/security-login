607b0a47c0b3af65b15ae112d1ca062f090946d1
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2021-01-22 19:15:26 -0300

    atualizacao oauth

--- src/main/java/com/solucitiva/solucitiva/auth/auth/AuthorizationServerConfig.java
+++ src/main/java/com/solucitiva/solucitiva/auth/auth/AuthorizationServerConfig.java
@@ -1,7 +1,11 @@
 package com.solucitiva.solucitiva.auth.auth;
 
+import com.nimbusds.jose.shaded.json.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +14,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.text.ParseException;
 
 @Configuration
 @EnableAuthorizationServer
@@ -26,6 +38,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        if(!isValid("6Lf72TQaAAAAAJFe2JG9P0nhmmdsrQefiddKkUdt")){
+//            return;
+//        }
         clients
                 .inMemory()
                 .withClient("contabil-web")
@@ -65,6 +80,65 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService)
                 .reuseRefreshTokens(false);
+    }
+
+    public boolean isValid(String clientRecaptchaResponse) throws IOException, ParseException, JSONException {
+        final String RECAPTCHA_SERVICE_URL = "https://www.google.com/recaptcha/api/siteverify";
+        final String SECRET_KEY = "6Lf72TQaAAAAAI2lW3dkZXCOpzuDy5cYAWIBPxDo";
+
+        if(clientRecaptchaResponse == null || "".equals(clientRecaptchaResponse)){
+            return false;
+        }
+
+        //conecta a url do google
+        URL url = new URL(RECAPTCHA_SERVICE_URL);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+
+        String postParams =
+                "secret=" + SECRET_KEY +
+                "&response=" + clientRecaptchaResponse;
+
+        //enviar POST para o google reCAPTCHA sever
+        connection.setDoOutput(true);
+        DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+        dataOutputStream.writeBytes(postParams);
+        dataOutputStream.flush();
+        dataOutputStream.close();
+
+        int responseCode = connection.getResponseCode();
+
+        //apagar - apenas para visualizar no console
+        System.out.println("Post parameters: " + postParams);
+        System.out.println("Response Code: " + responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                connection.getInputStream()));
+
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //apagar - apenas para visualizar no console
+        System.out.println(response.toString());
+
+        System.out.println(response.toString());
+
+        //Parse JSON-response
+        JSONObject jsonObject = new JSONObject(response.toString());
+
+        Boolean success = (Boolean) jsonObject.get("success");
+        Double score = (Double) jsonObject.get("score");
+
+        System.out.println("success : " + success);
+        System.out.println("score : " + score);
+
+        //result should be sucessfull and spam score above 0.5
+        return (success && score >= 0.5);
+
     }
 
 }
--- src/main/java/com/solucitiva/solucitiva/auth/auth/WebSecurityConfig.java
+++ src/main/java/com/solucitiva/solucitiva/auth/auth/WebSecurityConfig.java
@@ -2,13 +2,10 @@ package com.solucitiva.solucitiva.auth.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -46,22 +43,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.userDetailsService();
     }
 
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-//                .antMatchers("/**").permitAll()
-//                .anyRequest().fullyAuthenticated()
-//                .and()
-//                .httpBasic()
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and().cors()
-//                .and()
-//                .csrf().disable();
-//
-//    }
 }
